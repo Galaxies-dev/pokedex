e6a04560c76e78cbe000fd0b9c74a868e921b207
Simon Grimm <saimon@devdactic.com>
2026-01-21 12:44:25 +0100

    Add Pokemon details screen with loading and error handling; implement modal for viewing detailed stats.

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -7,3 +7,47 @@ export interface PokemonListResponse {
   count: number;
   results: Pokemon[];
 }
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: PokemonType[];
+  stats: PokemonStat[];
+  abilities: PokemonAbility[];
+  sprites: {
+    front_default: string;
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+}
